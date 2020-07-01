269834e0e7398dd0d3e354ac5d1712b681703713
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-01 15:16:57 +0900

    IOS에서 Dropdown 구현하기

--- dropDownPrac/ViewController.swift
+++ dropDownPrac/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import DropDown
 
 class ViewController: UIViewController {
-
+    var dropDown : DropDown?
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        dropDown = DropDown()
+        
+        // The view to which the drop down will appear on
+        dropDown!.anchorView = button // UIView or UIBarButtonItem
+        // Top of drop down will be below the anchorView
+        dropDown?.bottomOffset = CGPoint(x: 0, y:(dropDown?.anchorView?.plainView.bounds.height)!)
+        
+        // The list of items to display. Can be changed dynamically
+        dropDown!.dataSource = ["Car", "Motorcycle", "Truck"]
+        
+        button.addTarget(self, action: #selector(dropDownBtn), for: .touchUpInside)
+        dropDown?.selectionAction = { [unowned self] (index : Int, item : String) in
+            self.button.setTitle(item, for: .normal )
+        }
     }
-
+    @objc func dropDownBtn(){
+        dropDown?.show()
+    }
 
 }
 
